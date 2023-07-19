4caa2b611a593ee5148604000165c1880fb00276
Emily LD <lagreouemily@gmail.com>
2023-07-19 20:37:39 +0200

    Product detail component

---
+++ client/src/containers/BackOffice/ProductDetail.tsx
@@ -0,0 +1,111 @@
+import "../../components/modal.css";
+import CardItem from "../../components/CardItem";
+import {
+	faCalendar,
+	faCommentDots,
+	faLocationDot,
+	faRoute,
+	faShip,
+	faSmog,
+	faTag,
+	faWarehouse,
+} from "@fortawesome/free-solid-svg-icons";
+import { formatDate, formattedDate } from "../../utils/format-data-utils";
+import Button from "../../components/Button";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { DetailedProduct } from "../../dto/ProductDto";
+
+type ProductDetailProps = {
+	_id: string;
+};
+
+const ProductDetail = ({ _id }: ProductDetailProps) => {
+	const [product, setProduct] = useState<DetailedProduct | null>(null);
+	const [isLoading, setIsLoading] = useState(true);
+	const [creationDate, setCreationDate] = useState<formattedDate>("dd-mm-yyyy");
+
+	/** Read data associated with product _id on page loading */
+	useEffect(() => {
+		const fetchProduct = async () => {
+			const url = `http://localhost:8000/product/${_id}`;
+
+			try {
+				const response = await axios.get(url);
+				const productData = response.data;
+				if (!productData) {
+					console.error("No product was found");
+					throw new Error("Product not found");
+				}
+				setProduct(response.data.product);
+				const creationDate = response.data.product.creation_date;
+
+				if (creationDate) {
+					const convertDateFormat = formatDate(
+						new Date(creationDate),
+						"dd-mm-yyyy"
+					) as formattedDate;
+					setCreationDate(convertDateFormat);
+				}
+				setIsLoading(false);
+			} catch (error: any) {
+				console.error(error);
+				throw new Error(error.message);
+			}
+		};
+		fetchProduct();
+	}, [_id]);
+
+	// Functions to manipulate product data (modify / delete)
+	const deleteProduct = () => {
+		console.log("delete product");
+	};
+	const modifyProduct = () => {
+		console.log("modify product");
+	};
+
+	return (
+		<div>
+			{!isLoading && product && (
+				<div>
+					<h2 className="product-name">{product.name}</h2>
+					<CardItem text={`Marque : ${product.company}`} faIcon={faWarehouse} />
+					<CardItem text={`${product.co2}eq CO2`} faIcon={faSmog} />
+					<CardItem text={`Type de produit: ${product.type}`} faIcon={faTag} />
+					<CardItem
+						text={`Port d'origine: ${product.origin_harbour}`}
+						faIcon={faLocationDot}
+					/>
+					<CardItem
+						text={`Port d'arrivée: ${product.destination_harbour}`}
+						faIcon={faLocationDot}
+					/>
+					<CardItem text={`Distance parcourue: ${product.distance}`} faIcon={faRoute} />
+					{product.description && (
+						<CardItem
+							text={`Description: ${product.description}`}
+							faIcon={faCommentDots}
+						/>
+					)}
+					<CardItem
+						text={`Date de création de l'article : ${creationDate}`}
+						faIcon={faCalendar}
+					/>
+					<CardItem
+						text={`Type de transport: ${product.transportation}`}
+						faIcon={faShip}
+					/>
+					<div className="product-picture">
+						<img src="" alt="" />
+					</div>
+					<div className="product-modal-button">
+						<Button buttonText="Modifier" buttonType="button" callback={modifyProduct} />
+						<Button buttonText="Supprimer" buttonType="button" callback={deleteProduct} />
+					</div>
+				</div>
+			)}
+		</div>
+	);
+};
+
+export default ProductDetail;
