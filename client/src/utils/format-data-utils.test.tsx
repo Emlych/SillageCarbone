b74f0db8f102976a53c91aa1c75349caada12746
Emily LD <lagreouemily@gmail.com>
2023-08-03 21:00:20 +0200

    Test password strength

--- client/src/utils/format-data-utils.test.tsx
+++ client/src/utils/format-data-utils.test.tsx
@@ -2,6 +2,7 @@ import {
 	capitalizeFirstLetter,
 	formatDate,
 	formatTextToString,
+	isPasswordStrong,
 } from "./format-data-utils";
 
 /** Unit testing of utils function */
@@ -28,11 +29,46 @@ describe("Format Data Utils Function", () => {
 		expect(formattedDate).toBe("1-6-2023");
 	});
 
-	// First letter will be capital letter
+	// Format string to capital letters
 	test("First letter will be capital letter", () => {
 		const textToFormat = "boîte de thon gros massif";
 		const expectedText = "Boîte de thon gros massif";
 		const formattedText = capitalizeFirstLetter(textToFormat);
 		expect(formattedText).toEqual(expectedText);
+	});
+
+	// Password strength
+	test("Password without uppercase, but with lowercase, numbers and long -> weak password", () => {
+		const noUppercasePassword = "motdepasse123456789";
+		const isStrong = isPasswordStrong(noUppercasePassword);
+		expect(isStrong).toEqual(false);
+	});
+
+	// Password strength
+	test("Password without lowercase, but with uppercase, numbers and long -> weak password", () => {
+		const noLowercasePassword = "MOTDEPASSE123456789";
+		const isStrong = isPasswordStrong(noLowercasePassword);
+		expect(isStrong).toEqual(false);
+	});
+
+	// Password strength
+	test("Password without numbers, but with lowercase and uppercase, and long -> weak password", () => {
+		const noNumberPassword = "MOTDEPASSEMOTDEPASSE";
+		const isStrong = isPasswordStrong(noNumberPassword);
+		expect(isStrong).toEqual(false);
+	});
+
+	// Password strength
+	test("Password with numbers, lowercase and uppercase, but short (less than 12) -> weak password", () => {
+		const shortPassword = "Mm1";
+		const isStrong = isPasswordStrong(shortPassword);
+		expect(isStrong).toEqual(false);
+	});
+
+	// Password strength
+	test("Password with lowercase, uppercase, numbers and long -> strong password", () => {
+		const strongPassword = "MOTDEPASSE123456789motdepasse";
+		const isStrong = isPasswordStrong(strongPassword);
+		expect(isStrong).toEqual(true);
 	});
 });
