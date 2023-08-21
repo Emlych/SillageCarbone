bcfc13fb53a9504328a0aa94a16aae175421004f
Emily LD <lagreouemily@gmail.com>
2023-08-21 13:58:17 +0200

    BackOffice product unused code removed

--- client/src/containers/tests/BackOfficeProductCard.test.tsx
+++ client/src/containers/tests/BackOfficeProductCard.test.tsx
@@ -95,46 +95,4 @@ describe("Users Container", () => {
 
 		expect(openConfirmActionModal).toBeCalled();
 	});
-
-	// it("displays user cards after data is loaded", async () => {
-	// 	// Mock the return values of fetchUsers
-	// 	(fetchUsers as jest.Mock).mockResolvedValue({
-	// 		users: mockUsers,
-	// 		count: mockUsers.length,
-	// 	});
-	// 	render(<Users />);
-
-	// 	// Wait for user cards to be displayed
-	// 	const userCards = await screen.findAllByTestId("user-card");
-	// 	expect(userCards).toHaveLength(mockUsers.length);
-	// });
-
-	// it("updates filter input value when user types into the input field", () => {
-	// 	render(<Users />);
-	// 	// Find the input field by its data-testid
-	// 	const inputField = screen.getByTestId("search-word") as HTMLInputElement;
-	// 	// Simulate user typing into the input field
-	// 	fireEvent.change(inputField, { target: { value: "search text" } });
-	// 	// Check if the input field's value has been updated
-	// 	expect(inputField.value).toBe("search text");
-	// });
-
-	// it("updates start date filter when user selects a date in the DateInput component", async () => {
-	// 	// Mock the return values of fetchUsers
-	// 	(fetchUsers as jest.Mock).mockResolvedValue({
-	// 		users: mockUsers,
-	// 		count: mockUsers.length,
-	// 	});
-	// 	render(<Users />);
-	// 	// Find the start date input field by its type attribute
-	// 	const startDateInput = screen.getByTestId("custom-date-start");
-	// 	// Simulate user selecting a date in the input field
-	// 	fireEvent.change(startDateInput, { target: { value: "2023-05-10" } });
-
-	// 	// Wait for user cards to be displayed
-	// 	const userCardMails = await screen.findAllByTestId("user-card-mail");
-	// 	// Check if the filtered users are displayed
-	// 	const displayedUserEmails = userCardMails.map((card) => card.textContent);
-	// 	const expectedUserEmails = ["user3@example.com"]; // Only the third user matches the filter
-	// });
 });
