d38dc31dfbe07c5a1440944bf256ed3da1e94eef
Emily LD <lagreouemily@gmail.com>
2023-08-23 11:36:13 +0200

    console info for debug

--- client/src/containers/SearchProduct/SearchHero.container.tsx
+++ client/src/containers/SearchProduct/SearchHero.container.tsx
@@ -5,6 +5,14 @@ import "../../styling/searchhero.css";
 const SearchHero = () => {
 	return (
 		<div data-testid="search-hero" className="searchhero">
+			{/* <div className="backgroundImage">
+				<div className="svg-container">
+					<img src="image1.svg" alt="Image 1" />
+				</div>
+				<div className="svg-container">
+					<img src="image2.svg" alt="Image 2" />
+				</div>
+			</div> */}
 			<h2>Rechercher le bilan carbone d'un produit</h2>
 
 			{/* Search bar */}
