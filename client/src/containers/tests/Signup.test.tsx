84a83f8a367727b7aaa74c3ea93523a444b93059
Emily LD <lagreouemily@gmail.com>
2023-08-24 16:49:50 +0200

    Update unit testing for signup

--- client/src/containers/tests/Signup.test.tsx
+++ client/src/containers/tests/Signup.test.tsx
@@ -1,10 +1,18 @@
-import { render, fireEvent, screen } from "@testing-library/react";
+import { render, fireEvent, screen, waitFor } from "@testing-library/react";
 import Signup from "../Modal/Signup";
+import { createToken } from "../../utils/token-utils";
+import { createUser } from "../../services/userService";
+
+jest.mock("../../utils/token-utils", () => ({
+	createToken: jest.fn(),
+}));
+jest.mock("../../services/userService", () => ({
+	createUser: jest.fn(),
+}));
 
 describe("Signup", () => {
 	const mockSetUser = jest.fn();
 	const mockToggleModal = jest.fn();
-	//const mockHandleFormSubmit = jest.fn();
 	const mockSetMail = jest.fn();
 	const mockSetPassword = jest.fn();
 	const mockSetConfirmPassword = jest.fn();
@@ -29,7 +37,13 @@ describe("Signup", () => {
 		setComponentKeyName: mockSetComponentKeyName,
 	};
 
-	it("should update mail input value", () => {
+	it("renders correctly signup modal", () => {
+		render(<Signup {...defaultProps} />);
+		const title = screen.getByText("Création d'un compte");
+		expect(title).toBeInTheDocument();
+	});
+
+	it("Update mail input value", () => {
 		render(<Signup {...defaultProps} />);
 		const mailInput = screen.getByTestId("mail");
 		const mockMailValue = "test@example.com";
@@ -37,7 +51,7 @@ describe("Signup", () => {
 		expect(mockSetMail).toHaveBeenCalledWith(mockMailValue);
 	});
 
-	it("should update password input value", () => {
+	it("Update password input value", () => {
 		render(<Signup {...defaultProps} />);
 		const passwordInput = screen.getAllByTestId("password")[0];
 		const mockPasswordValue = "password123";
@@ -55,7 +69,7 @@ describe("Signup", () => {
 		expect(mockSetConfirmPassword).toHaveBeenCalledWith(mockConfirmPasswordValue);
 	});
 
-	it("should toggle hiddenPassword state when password eye icon is clicked", () => {
+	it("Toggle hiddenPassword state when password eye icon is clicked", () => {
 		render(<Signup {...defaultProps} />);
 		const passwordEyeIcon = screen.getAllByTestId("eye-icon")[0];
 		fireEvent.click(passwordEyeIcon);
@@ -63,7 +77,7 @@ describe("Signup", () => {
 		expect(mockSetHiddenPassword).toHaveBeenCalledWith(true);
 	});
 
-	it("should toggle hiddenConfirmPassword state when confirm password eye icon is clicked", () => {
+	it("Toggle hiddenConfirmPassword state when confirm password eye icon is clicked", () => {
 		render(<Signup {...defaultProps} />);
 		const confirmPasswordEyeIcon = screen.getAllByTestId("eye-icon")[1];
 		fireEvent.click(confirmPasswordEyeIcon);
@@ -71,18 +85,177 @@ describe("Signup", () => {
 		expect(mockSetHiddenConfirmPassword).toHaveBeenCalledWith(true);
 	});
 
-	// it.skip("should call handleFormSubmit when form is submitted", () => {
-	// 	render(<Signup {...defaultProps} />);
-	// 	const form = screen.getByTestId("signup-form");
-	// 	fireEvent.submit(form);
-	// 	expect(mockHandleFormSubmit).toHaveBeenCalledTimes(1);
-	// });
-
-	it('should set component key name to "login" when "Si vous avez déjà un compte, se connecter." is clicked', () => {
+	it('Set component key name to "login" when "Si vous avez déjà un compte, se connecter." is clicked', () => {
 		render(<Signup {...defaultProps} />);
 		const loginLink = screen.getByText("Si vous avez déjà un compte, se connecter.");
 		fireEvent.click(loginLink);
 		expect(mockSetComponentKeyName).toHaveBeenCalledTimes(1);
 		expect(mockSetComponentKeyName).toHaveBeenCalledWith("login");
+	});
+
+	it("Form submission with invalid mail should set error message", async () => {
+		render(
+			<Signup
+				toggleModal={jest.fn()}
+				//setUser={mockSetUser}
+				mail="invalid mail"
+				setMail={mockSetMail}
+				password="LongPassword1"
+				setPassword={mockSetPassword}
+				confirmPassword="LongPassword1"
+				setConfirmPassword={mockSetConfirmPassword}
+				hiddenPassword={false}
+				setHiddenPassword={mockSetHiddenPassword}
+				hiddenConfirmPassword={false}
+				setHiddenConfirmPassword={mockSetHiddenConfirmPassword}
+				setComponentKeyName={mockSetComponentKeyName}
+			/>
+		);
+
+		const form = screen.getByTestId("signup-form");
+		fireEvent.submit(form);
+
+		const errorMessage = screen.getByTestId("error-message");
+		const expectedMessage = "Veuillez fournir une adresse mail valide.";
+		expect(errorMessage.textContent).toBe(expectedMessage);
+	});
+
+	it("Valid form submission should call createToken", async () => {
+		const mockedCreateUser = createUser as jest.Mock;
+		const mockUserData = {
+			token: "newToken",
+			mail: "user@example.com",
+		};
+		mockedCreateUser.mockResolvedValue(mockUserData); // Mock a successful creation
+
+		const mockedCreateToken = createToken as jest.Mock;
+
+		render(
+			<Signup
+				toggleModal={jest.fn()}
+				mail="validMail@mail.com"
+				setMail={mockSetMail}
+				password="LongPassword1"
+				setPassword={mockSetPassword}
+				confirmPassword="LongPassword1"
+				setConfirmPassword={mockSetConfirmPassword}
+				hiddenPassword={false}
+				setHiddenPassword={mockSetHiddenPassword}
+				hiddenConfirmPassword={false}
+				setHiddenConfirmPassword={mockSetHiddenConfirmPassword}
+				setComponentKeyName={mockSetComponentKeyName}
+			/>
+		);
+
+		const form = screen.getByTestId("signup-form");
+		fireEvent.submit(form);
+
+		await waitFor(() => {
+			expect(mockedCreateToken).toHaveBeenCalledWith(
+				mockUserData.token,
+				mockUserData.mail,
+				false
+			);
+		});
+	});
+
+	it("Invalid user creation should set error message", async () => {
+		const mockedCreateUser = createUser as jest.Mock;
+		mockedCreateUser.mockRejectedValue(new Error("User could not be created")); // Mock an unsuccessful creation
+
+		render(
+			<Signup
+				toggleModal={jest.fn()}
+				mail="validMail@mail.com"
+				setMail={mockSetMail}
+				password="LongPassword1"
+				setPassword={mockSetPassword}
+				confirmPassword="LongPassword1"
+				setConfirmPassword={mockSetConfirmPassword}
+				hiddenPassword={false}
+				setHiddenPassword={mockSetHiddenPassword}
+				hiddenConfirmPassword={false}
+				setHiddenConfirmPassword={mockSetHiddenConfirmPassword}
+				setComponentKeyName={mockSetComponentKeyName}
+			/>
+		);
+
+		const form = screen.getByTestId("signup-form");
+		fireEvent.submit(form);
+
+		await waitFor(() => {
+			const errorMessage = screen.getByTestId("error-message");
+			const expectedMessage = "Erreur dans la création du compte.";
+			expect(errorMessage.textContent).toBe(expectedMessage);
+		});
+	});
+
+	it("Invalid user creation, because of existing user, should set error message", async () => {
+		const mockedCreateUser = createUser as jest.Mock;
+		const mockErrorResponse = {
+			response: {
+				data: {
+					message: "User already exists",
+				},
+			},
+		};
+		mockedCreateUser.mockRejectedValue(mockErrorResponse);
+
+		render(
+			<Signup
+				toggleModal={jest.fn()}
+				mail="validMail@mail.com"
+				setMail={mockSetMail}
+				password="LongPassword1"
+				setPassword={mockSetPassword}
+				confirmPassword="LongPassword1"
+				setConfirmPassword={mockSetConfirmPassword}
+				hiddenPassword={false}
+				setHiddenPassword={mockSetHiddenPassword}
+				hiddenConfirmPassword={false}
+				setHiddenConfirmPassword={mockSetHiddenConfirmPassword}
+				setComponentKeyName={mockSetComponentKeyName}
+			/>
+		);
+
+		const form = screen.getByTestId("signup-form");
+		fireEvent.submit(form);
+
+		await waitFor(() => {
+			const errorMessage = screen.getByTestId("error-message");
+			const expectedMessage = "Ce compte existe déjà.";
+			expect(errorMessage.textContent).toBe(expectedMessage);
+		});
+	});
+
+	it("toggles eye icon between faEye and faEyeSlash", () => {
+		const mockTogglePassword = jest.fn();
+		render(
+			<Signup
+				toggleModal={jest.fn()}
+				mail="validMail@mail.com"
+				setMail={mockSetMail}
+				password="LongPassword1"
+				setPassword={mockSetPassword}
+				confirmPassword="LongPassword1"
+				setConfirmPassword={mockSetConfirmPassword}
+				hiddenPassword={false}
+				setHiddenPassword={mockTogglePassword}
+				hiddenConfirmPassword={false}
+				setHiddenConfirmPassword={mockSetHiddenConfirmPassword}
+				setComponentKeyName={mockSetComponentKeyName}
+			/>
+		);
+
+		const eyeIcon = screen.getAllByTestId("eye-icon")[0];
+
+		// Initial state: hiddenPassword is true, should display faEyeSlash icon
+		expect(eyeIcon).toHaveAttribute("data-icon", "eye-slash");
+
+		// Simulate click event
+		fireEvent.click(eyeIcon);
+
+		// Confirm that setHiddenPassword was called with the updated value
+		expect(mockTogglePassword).toHaveBeenCalledWith(true);
 	});
 });
